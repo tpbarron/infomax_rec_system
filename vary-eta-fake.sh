ea566e5d050874030bd40010a54e76a7750448ab
Mark Strickland (Student) <ASUAD\mestric1@en4119351l.fulton.ad.asu.edu>
2017-11-27 13:00:44 -0700

    Adding recent changes to main and script

--- vary-eta-fake.sh
+++ vary-eta-fake.sh
@@ -3,9 +3,9 @@
 python3 main.py --eta 0.0 --use-fake-user --tag veta
 python3 main.py --eta 0.1 --use-fake-user --tag veta
 python3 main.py --eta 0.5 --use-fake-user --tag veta
-python3 main.py --eta 0.7 --use-fake-user --tag veta
-python3 main.py --eta 0.9 --use-fake-user --tag veta
 python3 main.py --eta 1.0 --use-fake-user --tag veta
-python3 main.py --eta 1.1 --use-fake-user --tag veta
-python3 main.py --eta 1.5 --use-fake-user --tag veta
-python3 main.py --eta 2.0 --use-fake-user --tag veta
+python3 main.py --eta 5.0 --use-fake-user --tag veta
+python3 main.py --eta 10.0 --use-fake-user --tag veta
+python3 main.py --eta 100.0 --use-fake-user --tag veta
+python3 main.py --eta 500.0 --use-fake-user --tag veta
+python3 main.py --eta 1000.0 --use-fake-user --tag veta
